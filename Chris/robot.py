1cf48f6ccd4589c2a754b35e3e59117c4a44af97
Chris Arnault <chris.arnault.1@gmail.com>
2025-03-19 16:33:18 +0100

    Simulation du programme robot.py - paramètre VILLES = nombre max de villes - implémentation de 2 algorithmes:   1) détection des villes à partir de la ville 1 jusqu'à la ville VILLES mais sans ordre intermédiaire   2) détection des villes dans l'ordre d'un contrat par exemple dans l'ordre range(1, VILLES)

--- Chris/robot.py
+++ Chris/robot.py
@@ -1,5 +1,7 @@
 import random, time, os
 
+VILLES = 6
+
 class Steps:
     def __init__(self):
         self.steps = dict()
@@ -15,32 +17,94 @@ class Steps:
         return self.steps[k]
     
 def motor():
-    return int(random.random()*100)
+    pass
 
-def detect():
-    return random.randint(1, 6)
-    
+def detect(t):
+    return random.randint(1, VILLES), t + int(random.random() * 100)
 
-history = Steps()
 
-t = time.time()
+def trajet1():
+    """
+    Premier algorithme:
+    - le véhicule démarre
+    - dès que l'on détecte la ville 1 on commence l'enregistrement de l'historique
+    - on enregistre chaque détection de ville quel que soit l'ordre
+    - on termine lorsque l'on détecte la ville VILLES
 
-s = 1
-while True:
-    # print("s = ", s)
-    town = detect()
-    t += motor()
-    history.addstep(s, town, t)
-    if town == 6:
-        break
-    s += 1
-    
-os.remove('history.txt')
+    Puis on écrit l'historique ordonné dans le fichier history.txt
+    """
+    history = Steps()
+
+    t = 0
+    s = 1
+
+    while True:
+        # print("s = ", s)
+        town, t = detect(t)
+        if town == 1:
+            history.addstep(s, 1, t)
+            s += 1
+            break
+
+    while True:
+        # print("s = ", s)
+        town, t = detect(t)
+        history.addstep(s, town, t)
+        if town == VILLES:
+            break
+        s += 1
+
+    return history
+
+
+def trajet2():
+    """
+    Deuxième algorithme:
+    - le véhicule démarre
+    - dès que l'on détecte la ville 1 on commence l'enregistrement de l'historique
+    - on enregistre chaque détection de ville à condition que l'on suive l'ordre de villes prévu
+    - on termine lorsque l'on détecte la ville VILLES
+
+    Puis on écrit l'historique ordonné dans le fichier history.txt
+    """
+    ordre = range(1, VILLES)
+
+    history = Steps()
+
+    t = 0
+    s = 1
+    contrat = 1
+
+    while True:
+        # print("s = ", s)
+        town, t = detect(t)
+        if town == contrat:
+            history.addstep(s, town, t)
+            s += 1
+            break
+
+    contrat += 1
+
+    while True:
+        # print("s = ", s)
+        town, t = detect(t)
+        if town == contrat:
+            history.addstep(s, town, t)
+            s += 1
+            contrat += 1
+            if town == VILLES:
+                break
+
+
+    return history
+
+
+history = trajet1()
 
 f = open("history.txt", "w+")
 
 for h in sorted(history.keys()):
-    print(f"history[{h}]={history.value(h)}\n")
+    print(f"history[{h}]={history.value(h)}")
     f.write(f"history[{h}]={history.value(h)}\n")
 f.close()
 
