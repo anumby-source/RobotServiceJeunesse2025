d41fe80866ae51de6463927188e2bb0f7ed5fe4a
gildeb <48280441+gildeb@users.noreply.github.com>
2025-03-21 09:37:59 +0100

    Add files via upload

--- Gilles/master/base.py
+++ Gilles/master/base.py
@@ -1,29 +1,32 @@
 import sys
 import network
 import espnow
+from time import sleep_ms
 
-# mac = b'$X|\x91\xe0\xd8'
-# A WLAN interface must be active to send()/recv()
+# mac = b'$X|\x91\xe0\xd8'  # base MAC address
+# espnow init
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
 e = espnow.ESPNow()
 e.active(True)
 print('espnow ok')
+# get nbplayer from master
+while (nbplayer := int(input())) not in (1, 2):
+    pass
 #
-nbplayer = int(input())
-addr_list = []
-
-while len(addr_list) < nbplayer:
+addr_list = []     #  player robot MAC address list
+#  wait for robots connections
+while (len(addr_list) < nbplayer):
     addr, msg = e.recv()
     if addr not in addr_list:
         e.add_peer(addr)
         addr_list.append(addr)
         sys.stdout.write(msg)
-#
+#  wait for start command from master
 cmd = input()
 if cmd == 'start':
     e.send(cmd)
-#
+#  receive robots messages and forward to master
 while True:
     try:
         if e.any():
