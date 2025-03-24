a0ba0aa98d42ac4a5ff7efee129f773de83f6ffe
gildeb <48280441+gildeb@users.noreply.github.com>
2025-03-24 17:06:16 +0100

    Add files via upload

--- Gilles/telecommande/main.py
+++ Gilles/telecommande/main.py
@@ -1,10 +1,12 @@
 # Put your code here
 #################################
-def start_joystick(e):
-    import telecommande
 
-from machine import Pin
-irq = Pin(9, Pin.IN).irq(handler=start_joystick, trigger=Pin.IRQ_FALLING)
+# def start_telecommande(e):
+#     Pin(9, Pin.IN).irq(handler=None, trigger=Pin.IRQ_FALLING)
+#     print("Main.py : loading telecommande.py")
+#     import telecommande
+#
+# from machine import Pin
+# irq = Pin(9, Pin.IN).irq(handler=start_telecommande, trigger=Pin.IRQ_FALLING)
 #################################
-
-
+import telecommande
--- Gilles/telecommande/telecommande.py
+++ Gilles/telecommande/telecommande.py
@@ -2,7 +2,9 @@ import network
 import espnow
 from machine import Pin, ADC
 from time import sleep_ms, ticks_ms
+from neopixel import NeoPixel
 #
+red, green, blue, white, black = (0, 128, 0), (128, 0, 0), (0, 0, 128), (128, 128, 128), (0, 0, 0)
 def normalize(x, amp=100):
     ''' resize x -> [-amp;+amp] '''
     x -= midadc
@@ -27,6 +29,7 @@ def change_mode():
 #
 def constrain(x):
     return min(max(x, -100), 100)
+
 # init ADC
 a0 = ADC(0, atten=ADC.ATTN_11DB)
 a1 = ADC(1, atten=ADC.ATTN_11DB)
@@ -40,6 +43,10 @@ ramp   = 30
 # init onbard led
 led = Pin(8, Pin.OUT)
 
+# init neopixel
+np = NeoPixel(Pin(5, Pin.OUT),1)
+np[0] = white
+np.write()
 # init joystick click
 p2 = Pin(2, Pin.IN)
 pressed = False
@@ -53,12 +60,13 @@ sta.disconnect()      # For ESP8266
 e = espnow.ESPNow()
 e.active(True)
 print("telecommande.py : Network active, Espnow ok")
-robot = b'\x24\x58\x7c\x91\xe3\x60'
-e.add_peer(robot)      # Must add_peer() before send()
+# peer = b'\xe8\x06\x90\x66\x68\x0c'   # display pannel MAC address
+robotAddr = b'\x24\x58\x7c\x91\xe3\x60'
+try:
+    e.add_peer(robotAddr)      # Must add_peer() before send()
+except:
+    pass
 print("telecommande.py : robot added to peers")
-#
-while not e.send("Starting..."):
-    sleep_ms(200)
 #
 while True:
     try:
@@ -68,7 +76,7 @@ while True:
         ls, rs = constrain(s+r), constrain(s-r)
         cmd = 'ml.set_speed(' + str(ls) + ')\r'
         cmd += 'mr.set_speed(' + str(rs) + ')\r'
-        e.send(cmd.encode())
+        e.send(robotAddr, cmd.encode(), False)
         print(cmd.encode())
         sleep_ms(100)
         if (s < samp - 2) and (not p2.value()): # check and debounce click
@@ -79,5 +87,6 @@ while True:
         else:
             pressed = False
     except:
-        led.off()
+        np[0] = red
+        np.write()
         break
