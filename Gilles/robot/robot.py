3933a3bd04c8aba8036366bfb395131ebc32dde6
gildeb <48280441+gildeb@users.noreply.github.com>
2025-03-24 13:48:45 +0100

    Add files via upload

--- Gilles/robot/robot.py
+++ Gilles/robot/robot.py
@@ -1,6 +1,6 @@
 ###################################################################
 #
-#   Command car with ESP32-C3/ESPnow mini joystick
+#   RSJ2025 : Robot code
 #
 ###################################################################
 
@@ -14,6 +14,7 @@ from struct import pack
 
 #
 robot_num = 1
+liste_pan = ['Paris', 'Auxerre', 'Lyon', 'Marseille']
 # motors initialization
 ml = dcMotor(pin1=5, pin2=6, pinEn=7, freq=10000)      # left motor
 mr = dcMotor(pin1=0, pin2=1, pinEn=2, freq=10000)      # right motor
@@ -26,7 +27,6 @@ led = Pin(8, Pin.OUT)
 # UART initialization
 led.off()     # led on
 u = UART(0, rx=Pin(20, Pin.IN), tx=Pin(21, Pin.OUT))
-# print("robot : UART init ok")
 #
 led.on()      # led off
 sleep_ms(1000)
@@ -41,22 +41,19 @@ e.active(True)
 print("robot : espnow init ok")
 # wait for joystick
 joyAddr, msg = e.recv()   # wait for joystick wakeup
-e.add_peer(joyAddr)
+try:
+    e.add_peer(joyAddr)
+except:
+    pass     # if joyAddr already in peer list
 print(b"robot : message telecommande=" + msg)
 # connect to base
 baseAddr = b'$X|\x91\xe0\xd8' #  display panel Mac Address
-e.add_peer(baseAddr)
-while not e.send(baseAddr, "ROBOT {}".format(robot_num)):
-    sleep_ms(1000)
-print('base ok')
+try:
+    e.add_peer(baseAddr)
+except:
+    pass      # if baseAddr already in peer list
 #
 led.on()      # led off
-# wait for start signal
-while True:
-    addr, cmd = e.recv()
-    if (addr == baseAddr) and (cmd == b'start'):
-        print(cmd)
-        break
 #
 u.read(u.any())   # empty uart buffer
 msg = b''
@@ -64,15 +61,15 @@ msg = b''
 while True:
     if u.any():
         msg += u.read(u.any())
-        if msg[-1] == 0x0a:
+        if msg[-1] == 0x0a:  # last char = '\n'
             e.send(baseAddr, msg)
             print(msg)
-            msg = b''
-#         
+            msg = b''       
     try:
         addr, cmd = e.recv()
+#         print(cmd)
         exec(cmd)
     except:
         print(b"robot : error command:" + cmd)
-        led.off()
+        led.off()    # led on
         break
