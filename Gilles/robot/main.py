e47ea1c04577023a24d7878477f6349b9f035c25
gildeb <48280441+gildeb@users.noreply.github.com>
2025-03-25 11:39:09 +0100

    Add files via upload

--- Gilles/robot/main.py
+++ Gilles/robot/main.py
@@ -1,11 +1,13 @@
 # Put your code here
 
 #################################
-def start_robot(e):
-    Pin(9, Pin.IN).irq(handler=None, trigger=Pin.IRQ_FALLING)
-    import robot
-        
-from machine import Pin
-irq = Pin(9, Pin.IN).irq(handler=start_robot, trigger=Pin.IRQ_FALLING)
+# def start_robot(e):
+#     Pin(9, Pin.IN).irq(handler=None, trigger=Pin.IRQ_FALLING)
+#     import robot
+#
+# from machine import Pin
+# irq = Pin(9, Pin.IN).irq(handler=start_robot, trigger=Pin.IRQ_FALLING)
 #################################
 # mac = b'$X|\x91\xe3`'   # this device
+
+import robot
--- Gilles/robot/robot.py
+++ Gilles/robot/robot.py
@@ -15,23 +15,28 @@ from struct import pack
 #
 robot_num = 1
 liste_pan = ['Paris', 'Auxerre', 'Lyon', 'Marseille']
+baseAddr  = b'$X|\x91\xe0\xd8' #  base Mac Address
+joyAddr   = b'\xa0\x85\xe3\x1aX\xb8'  #  joystick mac address
+
 # motors initialization
 ml = dcMotor(pin1=5, pin2=6, pinEn=7, freq=10000)      # left motor
 mr = dcMotor(pin1=0, pin2=1, pinEn=2, freq=10000)      # right motor
 ml.duty_offset = 40
 mr.duty_offset = 40
 print("robot : motor init ok")
+
 # led initialization
 led = Pin(8, Pin.OUT)
+led.off()     # led on
+sleep_ms(1000)
+led.on()      # led off
+print("robot : led init ok")
 
 # UART initialization
-led.off()     # led on
 u = UART(0, rx=Pin(20, Pin.IN), tx=Pin(21, Pin.OUT))
-#
-led.on()      # led off
-sleep_ms(1000)
+print("robot : UART init ok")
+
 # espnow initialization
-led.off()     # led on
 sta = network.WLAN(network.STA_IF) # A WLAN interface must be active
 sta.active(True)
 sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
@@ -39,22 +44,21 @@ print("robot : WLAN init ok")
 e = espnow.ESPNow()
 e.active(True)
 print("robot : espnow init ok")
-# wait for joystick
-joyAddr, msg = e.recv()   # wait for joystick wakeup
+
+# add joystick and base to peers
 try:
     e.add_peer(joyAddr)
 except:
-    pass     # if joyAddr already in peer list
-print(b"robot : message telecommande=" + msg)
-# connect to base
-baseAddr = b'$X|\x91\xe0\xd8' #  display panel Mac Address
+    pass         # if joyAddr already in peer list
+print(b"robot : joystick address added")
+# 
 try:
     e.add_peer(baseAddr)
 except:
-    pass      # if baseAddr already in peer list
+    pass         # if baseAddr already in peer list
+print(b"robot : base address added")
 #
-led.on()      # led off
-#
+
 u.read(u.any())   # empty uart buffer
 msg = b''
 #
@@ -66,9 +70,10 @@ while True:
             print(msg)
             msg = b''       
     try:
-        addr, cmd = e.recv()
+        addr, cmd = e.recv(0)
 #         print(cmd)
-        exec(cmd)
+        if addr == joyAddr:
+            exec(cmd)
     except:
         print(b"robot : error command:" + cmd)
         led.off()    # led on
