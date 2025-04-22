f39998137c69f9da7fa480385c6166ac9c750dc5
gildeb <48280441+gildeb@users.noreply.github.com>
2025-04-22 18:10:49 +0200

    Add files via upload

--- Gilles/base/base.py
+++ Gilles/base/base.py
@@ -6,6 +6,9 @@ from machine import Pin
 from mac_addr import robot_mac
 
 # mac = b'$X|\x91\xe0\xd8'  # base MAC address
+mac_robot = {}
+for e in robot_mac.items():
+    mac_robot[e[1]] = e[0]
 # espnow init
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
@@ -17,6 +20,11 @@ led = Pin(8, Pin.OUT)
 led.on()    # led off
 #
 while True:
-  addr, msg = e.recv(0)
+  addr, msg = e.recv()
   if addr:
-      print("{}:{}".format(robot_mac[addr], msg.decode()))
+#      print('msg:', msg)
+      l = msg.split(b'\n')
+      if len(l) == 2:
+          ville = l[0]
+#          print('ville:', ville)
+          print("{}:{}".format(mac_robot[addr], ville.decode()))
