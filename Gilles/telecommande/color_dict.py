a1ef618a3c8699508dd408ba427e62f65fc992d8
gildeb <48280441+gildeb@users.noreply.github.com>
2025-04-22 18:11:40 +0200

    Add files via upload

---
+++ Gilles/telecommande/color_dict.py
@@ -0,0 +1,7 @@
+color = {
+    b'Paris\n'     : (0, 128, 0),
+    b'Sens\n'      : (32, 100, 0),
+    b'Lyon\n'      : (64, 0, 0),
+    b'Marseille\n' : (0, 0, 128), 
+    b'Toulon\n'    : (0, 64, 64)
+    }
